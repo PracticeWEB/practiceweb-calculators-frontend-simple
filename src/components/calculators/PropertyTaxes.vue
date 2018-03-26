b12f7168bef942c1f3c0914ad10f0a03c2626d77
David Robinson <David.Robinson@practiceweb.co.uk>
2018-03-26 14:50:20 +0100

    Added first time buyer question to property taxes Added computed property for the duty name.

--- src/components/calculators/PropertyTaxes.vue
+++ src/components/calculators/PropertyTaxes.vue
@@ -4,6 +4,7 @@ Property Taxes covers both Stamp Duty and Land  & Buildings Transaction Tax ( LB
 -->
 <template>
     <div class="practiceweb-calculator">
+        <span>{{ dutyName }}</span>
         <form @submit.prevent="submitCalculation">
             <fieldset>
                 <legend>Region:</legend>
@@ -16,6 +17,11 @@ Property Taxes covers both Stamp Duty and Land  & Buildings Transaction Tax ( LB
                 <label>Residential<input type="radio" v-model="input.type" value="residential"></label>
                 <label>Non-residential<input type="radio" v-model="input.type" value="nonresidential"></label>
             </fieldset>
+            <fieldset v-if="input.type == 'residential'">
+                <legend>Are you a first time buyer?</legend>
+                <label>Yes <input type="radio" v-model="input.firstTime" value="yes" /></label>
+                <label>No <input type="radio" v-model="input.firstTime" value="no" /></label>
+            </fieldset>
             <label>
                 Property Value
                 <input type="number" v-model.number="input.propertyValue"/>
@@ -24,7 +30,7 @@ Property Taxes covers both Stamp Duty and Land  & Buildings Transaction Tax ( LB
         </form>
         <div v-if="Object.keys(output).length > 0">
             <ul>
-                <li><span> Duty: {{ output.duty | currency }}</span></li>
+                <li><span> {{ dutyName }} : {{ output.duty | currency }}</span></li>
                 <li v-if="output.secondHomeDuty"><span>Second Home Duty: {{ output.secondHomeDuty | currency }}</span></li>
             </ul>
         </div>
@@ -45,9 +51,23 @@ export default {
       input: {
         region: 'england',
         type: 'residential',
-        propertyValue: '300000'
+        propertyValue: '300000',
+        firstTime: 'no'
       },
       output: {}
+    }
+  },
+  computed: {
+    dutyName: function () {
+      let dutyName = 'Stamp Duty'
+      switch (this.input.region) {
+        case 'wales' :
+          dutyName = 'Land Transaction Tax'
+          break
+        case 'scotland' :
+          dutyName = 'Land and Buildings Transaction Tax'
+      }
+      return dutyName
     }
   }
 }

89fa4d9e59cea3afa8f118ca392c7040a7a9a272
Kevin Chilcott <kchilcott@MAC411-2.local>
2018-10-04 14:01:32 +0100

    Made structural changes too both property tax and VAT calculators, as well as stying amends too always be full width

--- src/components/calculators/PropertyTaxes.vue
+++ src/components/calculators/PropertyTaxes.vue
@@ -4,64 +4,64 @@ Property Taxes covers both Stamp Duty and Land  & Buildings Transaction Tax ( LB
 -->
 <template>
     <div class="practiceweb-calculator">
-        <div class="practiceweb-calculator-header--width">
-          <h3 class="practiceweb-calculator-header">{{ dutyName }}</h3>
-        </div>
-        <form class="input input--width" @submit.prevent="submitCalculation">
-          <fieldset class="input-fields">
-            <p class="input-fields__title">Region:</p>
-            <div class="form-group form-group--mobile flex-wrap">
-              <label class="input-field--label radio-spacing"  :class="{active: input.region === 'england'}">
-                <input type="radio" v-model="input.region" value="england" class="input-field input-field--radio" :checked="checked"/>England & Northern Ireland
-              </label>
-              <label class="input-field--label radio-spacing"  :class="{active: input.region === 'wales'}">
-                <input type="radio" v-model="input.region" value="wales" class="input-field input-field--radio" :checked="checked"/>Wales
-              </label>
-              <label class="input-field--label radio-spacing"  :class="{active: input.region === 'scotland'}">
-                <input type="radio" v-model="input.region" value="scotland" class="input-field input-field--radio" :checked="checked"/>Scotland
-              </label>
-            </div>
-          </fieldset>
-          <fieldset class="input-fields">
-            <p class="input-fields__title">Type of property:</p>
-            <div class="form-group form-group--mobile flex-wrap">
-              <label class="input-field--label radio-spacing"  :class="{active: input.type === 'residential'}">
-                <input type="radio" v-model="input.type" value="residential" class="input-field input-field--radio" :checked="checked"/>Residential
-              </label>
-              <label class="input-field--label radio-spacing"  :class="{active: input.type === 'nonresidential'}">
-                <input type="radio" v-model="input.type" value="nonresidential" class="input-field input-field--radio" :checked="checked"/>Non-residential
-              </label>
-            </div>
-          </fieldset>
-          <fieldset class="input-fields">
-            <p class="input-fields__title">Are you a first time buyer?</p>
-            <div class="form-group form-group--mobile flex-wrap">
-              <label class="input-field--label radio-spacing"  :class="{active: input.firstTime === 'yes'}">
-                <input type="radio" v-model="input.firstTime" value="yes" class="input-field input-field--radio" :checked="checked"/>Yes
-              </label>
-              <label class="input-field--label radio-spacing"  :class="{active: input.firstTime === 'no'}">
-                <input type="radio" v-model="input.firstTime" value="no" class="input-field input-field--radio" :checked="checked"/>No
-              </label>
-            </div>
-          </fieldset>
-          <fieldset class="input-fields">
-            <p class="input-fields__title">Property Value</p>
-            <div class="form-group form-group--gross flex-wrap">
-              <input name="propertyValue" v-validate="'required|decimal:2'" type="number" min="1" step="0.01" v-model.number="input.propertyValue" class="input-field input-field--number" placeholder="Enter your property value here"/>
-            </div>
-          </fieldset>
-          <button type="submit" :class="classes.button">Calculate</button>
-        </form>
-        <div class="output" v-if="Object.keys(output).length > 0">
-          <div class="output-list inline">
-            <p class="output-list--header">Breakdown</p>
-            <div class="property-output">
-              <p class="value-label inline">{{ dutyName }}:</p>
-              <p class="value-items inline">{{ output.duty | currency }}</p>
-            </div>
-            <div class="property-output" v-if="output.secondHomeDuty">
-              <p class="value-label inline">Second Home Duty:</p>
-              <p class="value-items inline">{{ output.secondHomeDuty | currency }}</p>
+        <div class="content-container">
+          <div class="practiceweb-calculator-header--width">
+            <h3 class="practiceweb-calculator-header">{{ dutyName }}</h3>
+          </div>
+          <form class="input" @submit.prevent="submitCalculation">
+            <fieldset class="input-fields">
+              <p class="input-fields__title">Region:</p>
+              <div class="form-group form-group--mobile flex-wrap">
+                <label class="input-field--label radio-spacing"  :class="{active: input.region === 'england'}">
+                  <input type="radio" v-model="input.region" value="england" class="input-field input-field--radio" :checked="checked"/>England & Northern Ireland
+                </label>
+                <label class="input-field--label radio-spacing"  :class="{active: input.region === 'wales'}">
+                  <input type="radio" v-model="input.region" value="wales" class="input-field input-field--radio" :checked="checked"/>Wales
+                </label>
+                <label class="input-field--label radio-spacing"  :class="{active: input.region === 'scotland'}">
+                  <input type="radio" v-model="input.region" value="scotland" class="input-field input-field--radio" :checked="checked"/>Scotland
+                </label>
+              </div>
+            </fieldset>
+            <fieldset class="input-fields">
+              <p class="input-fields__title">Type of property:</p>
+              <div class="form-group form-group--mobile flex-wrap">
+                <label class="input-field--label radio-spacing"  :class="{active: input.type === 'residential'}">
+                  <input type="radio" v-model="input.type" value="residential" class="input-field input-field--radio" :checked="checked"/>Residential
+                </label>
+                <label class="input-field--label radio-spacing"  :class="{active: input.type === 'nonresidential'}">
+                  <input type="radio" v-model="input.type" value="nonresidential" class="input-field input-field--radio" :checked="checked"/>Non-residential
+                </label>
+              </div>
+            </fieldset>
+            <fieldset class="input-fields">
+              <p class="input-fields__title">Are you a first time buyer?</p>
+              <div class="form-group form-group--mobile flex-wrap">
+                <label class="input-field--label radio-spacing"  :class="{active: input.firstTime === 'yes'}">
+                  <input type="radio" v-model="input.firstTime" value="yes" class="input-field input-field--radio" :checked="checked"/>Yes
+                </label>
+                <label class="input-field--label radio-spacing"  :class="{active: input.firstTime === 'no'}">
+                  <input type="radio" v-model="input.firstTime" value="no" class="input-field input-field--radio" :checked="checked"/>No
+                </label>
+              </div>
+            </fieldset>
+            <fieldset class="input-fields">
+              <p class="input-fields__title">Property Value</p>
+              <div class="form-group form-group--gross flex-wrap">
+                <input name="propertyValue" v-validate="'required|decimal:2'" type="number" min="1" step="0.01" v-model.number="input.propertyValue" class="input-field input-field--number" placeholder="Enter your property value here"/>
+              </div>
+            </fieldset>
+            <button type="submit" class="calculate-button" :class="classes.button">Calculate</button>
+          </form>
+          <br>
+          <div class="output" v-if="Object.keys(output).length > 0">
+            <div class="output-list">
+              <div class="sub-values flex flex-wrap">
+                <dt class="value-label inline">{{ dutyName }}:</dt>
+                <dd class="value-items inline">{{ output.duty | currency }}</dd>
+                <dt class="value-label inline property-output" v-if="output.secondHomeDuty">Second Home Duty:</dt>
+                <dd class="value-items inline" v-if="output.secondHomeDuty">{{ output.secondHomeDuty | currency }}</dd>
+              </div>
             </div>
           </div>
         </div>
--- src/components/calculators/Vat.vue
+++ src/components/calculators/Vat.vue
@@ -2,46 +2,71 @@
 Vat calculator component.
 -->
 <template>
-    <div class="calculator-container practiceweb-calculator">
-    <form @submit.prevent="submitCalculation">
-        <label>
-            Value
-            <input type="number" step="0.01" v-model.number="input.value"/>
-        </label>
-        <label>
-            VAT included?
-            Yes <input type="radio" v-model="input.included" value="1"/>
-            No <input type="radio" v-model="input.included" value="0"/>
-        </label>
-        <button type="submit">Calculate</button>
-    </form>
-    <div v-if="Object.keys(output).length > 0">
-        <ul>
-            <li><span>Net: {{ output.net | currency }} </span></li>
-            <li><span>VAT: {{ output.vat | currency }} </span></li>
-            <li><span>Gross: {{ output.gross | currency }} </span></li>
-        </ul>
+  <div class="calculator-container practiceweb-calculator">
+    <div class="content-container">
+      <form class="input" @submit.prevent="submitCalculation">
+        <div class="practiceweb-calculator-header--width">
+          <h3 class="practiceweb-calculator-header">VAT Calculator</h3>
+        </div>
+        <fieldset class="input-fields">
+          <p class="input-fields__title">Value *</p>
+          <div class="form-group form-group--gross flex-wrap">
+            <span class="input-field--specialCharacter input-field--specialCharacter--pound">&#163;</span>
+            <input class="input-field input-field--number border-left" type="number" step="0.01" v-model.number="input.value" placeholder="Enter your value here"/>
+          </div>
+        </fieldset>
+        <fieldset class="input-fields">
+          <p class="input-fields__title">VAT included? *</p>
+          <div class="form-group form-group--mobile flex-wrap">
+            <label class="input-field--label radio-spacing" :class="{active: input.included === '1'}">
+              <input type="radio" v-model="input.included" value="1" :checked="checked"/>Yes
+            </label>
+            <label class="input-field--label radio-spacing" :class="{active: input.included === '0'}">
+              <input type="radio" v-model="input.included" value="0" :checked="checked"/>No
+            </label>
+          </div>
+        </fieldset>
+        <button type="submit" class="calculate-button" :class="classes.button">Calculate</button>
+      </form>
+      <br>
+      <div class="output" v-if="Object.keys(output).length > 0">
+        <div class="output-list">
+          <div class="sub-values flex flex-wrap">
+            <dt class="value-items value-label">Net:</dt>
+            <dd class="value-items value-number">{{ output.net | currency }}</dd>
+            <dt class="value-items value-label">VAT:</dt>
+            <dd class="value-items value-number">{{ output.vat | currency }}</dd>
+            <dt class="value-items value-label">Gross:</dt>
+            <dd class="value-items value-number">{{ output.gross | currency }}</dd>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-
-import CalculatorAPIMixin from '../../mixins/CalculatorAPI'
-export default {
-  name: 'Vat',
-  mixins: [CalculatorAPIMixin],
-  created: function () {
-    this.servicePath = '/calculator/vat'
-  },
-  data: function () {
-    return {
-      input: {
-        included: 1,
-        value: 0
-      },
-      output: {}
+  import CalculatorAPIMixin from '../../mixins/CalculatorAPI'
+  import CalculatorStyle from '../../mixins/CalculatorStyle'
+  export default {
+    name: 'Vat',
+    mixins: [CalculatorAPIMixin, CalculatorStyle],
+    created: function () {
+      this.servicePath = '/calculator/vat'
+      this.tabNames = {
+        net: 'Net Pay',
+        vat: 'VAT',
+        gross: 'Gross'
+      }
+    },
+    data: function () {
+      return {
+        input: {
+          included: 1,
+          value: 0
+        },
+        output: {}
+      }
     }
   }
-}
 </script>

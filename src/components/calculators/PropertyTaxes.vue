fc7a2a2fb66fef3c9b6d37dcd25420b57e88b416
Kevin Chilcott <kchilcott@MAC411-2.local>
2018-10-09 15:14:23 +0100

    Final amends and adding of global sass file too allow for single file sass updates. Committing for pull request

--- src/components/calculators/PropertyTaxes.vue
+++ src/components/calculators/PropertyTaxes.vue
@@ -65,35 +65,9 @@ Property Taxes covers both Stamp Duty and Land  & Buildings Transaction Tax ( LB
             </div>
           </div>
         </div>
-        <!--<div v-if="Object.keys(output).length > 0">-->
-            <!--<ul>-->
-                <!--<li><span> {{ dutyName }} : {{ output.duty | currency }}</span></li>-->
-                <!--<li v-if="output.secondHomeDuty"><span>Second Home Duty: {{ output.secondHomeDuty | currency }}</span></li>-->
-            <!--</ul>-->
-        <!--</div>-->
     </div>
 </template>
 
-<!--<fieldset>-->
-<!--<legend>Region:</legend>-->
-<!--<label>England & Northern Ireland<input type="radio" v-model="input.region" value="england"/></label>-->
-<!--<label>Wales <input type="radio" v-model="input.region" value="wales"/></label>-->
-<!--<label>Scotland <input type="radio" v-model="input.region" value="scotland"/></label>-->
-<!--</fieldset>-->
-<!--<fieldset>-->
-<!--<legend>Type of property</legend>-->
-<!--<label>Residential<input type="radio" v-model="input.type" value="residential"></label>-->
-<!--<label>Non-residential<input type="radio" v-model="input.type" value="nonresidential"></label>-->
-<!--</fieldset>-->
-<!--<fieldset v-if="input.type == 'residential'">-->
-<!--<legend>Are you a first time buyer?</legend>-->
-<!--<label>Yes <input type="radio" v-model="input.firstTime" value="yes" /></label>-->
-<!--<label>No <input type="radio" v-model="input.firstTime" value="no" /></label>-->
-<!--</fieldset>-->
-<!--<label>-->
-<!--Property Value-->
-<!--<input type="number" v-model.number="input.propertyValue"/>-->
-<!--</label>-->
 <script>
 
 import CalculatorAPIMixin from '../../mixins/CalculatorAPI'

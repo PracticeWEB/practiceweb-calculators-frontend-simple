8e4bf51e6b4394476623a409f15850ca2f5c24b8
Kevin Chilcott <kchilcott@MAC411-2.local>
2018-10-12 10:57:52 +0100

    Added structure and styling too the final calculator which is the Fuel cost calculator

--- src/components/calculators/FuelCost.vue
+++ src/components/calculators/FuelCost.vue
@@ -3,15 +3,47 @@ Fuel Cost calculator.
 -->
 <template>
     <div class="practiceweb-calculator">
-        <form @submit.prevent="submitCalculation">
-            <label>Annual Mileage: <input type="number" min="1" v-model.number="input.annualMileage" step="0.01"></label>
-            <label>Price per Litre: <input type="number" min="1" v-model.number="input.pricePerLitre"></label>
-            <label>Miles per Gallon: <input type="number" min="1" v-model.number="input.milesPerGallon" step="0.01"></label>
-            <button type="submit" :class="classes.button">Calculate</button>
-        </form>
-        <div v-if="Object.keys(output).length > 0">
-            <span>Fuel Cost: {{ output.fuelCost | currency }} </span>
+      <div class="content-container">
+        <div class="practiceweb-calculator-header--width">
+          <h3 class="practiceweb-calculator-header">Fuel Cost Calculator</h3>
         </div>
+        <form @submit.prevent="submitCalculation">
+          <fieldset class="input-fields">
+            <p class="input-fields__title">Annual Mileage:</p>
+            <div class="form-group form-group--gross flex-wrap">
+              <input type="number" min="1" v-model.number="input.annualMileage" step="0.01" class="input-field input-field--number">
+            </div>
+          </fieldset>
+          <fieldset class="input-fields">
+            <p class="input-fields__title">Price per Litre:</p>
+            <div class="form-group form-group--gross flex-wrap">
+              <input type="number" min="1" v-model.number="input.pricePerLitre" class="input-field input-field--number">
+            </div>
+          </fieldset>
+          <fieldset class="input-fields">
+            <p class="input-fields__title">Miles per Gallon:</p>
+            <div class="form-group form-group--gross flex-wrap">
+              <input type="number" min="1" v-model.number="input.milesPerGallon" step="0.01" class="input-field input-field--number">
+            </div>
+          </fieldset>
+          <!---->
+            <!--<label>Annual Mileage: <input type="number" min="1" v-model.number="input.annualMileage" step="0.01"></label>-->
+            <!--<label>Price per Litre: <input type="number" min="1" v-model.number="input.pricePerLitre"></label>-->
+            <!--<label>Miles per Gallon: <input type="number" min="1" v-model.number="input.milesPerGallon" step="0.01"></label>-->
+            <button type="submit" class="calculate-button" :class="classes.button">Calculate</button>
+        </form>
+        <br>
+        <div class="output" v-if="Object.keys(output).length > 0">
+          <div class="output-list">
+            <div class="sub-values flex flex-wrap">
+              <dt class="value-label inline">Fuel Cost:</dt>
+              <dd class="value-items inline">{{ output.fuelCost | currency }}</dd>
+            </div>
+          </div>
+        <!--<div v-if="Object.keys(output).length > 0">-->
+            <!--<span>Fuel Cost: {{ output.fuelCost | currency }} </span>-->
+        </div>
+      </div>
     </div>
 </template>
 

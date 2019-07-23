1938ec8b979ee28eb2a7e97fc5ea56db6306d80a
David Robinson <David.Robinson@practiceweb.co.uk>
2019-07-23 16:53:24 +0100

    Set default for publicPath and went back to default webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,8 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.PUBLIC_PATH,
+  publicPath: process.env.PUBLIC_PATH || '/',
   filenameHashing: true,
   css: {
     extract: false
-  },
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 200000
-      }
-    }
   }
 }
